1c54a68c1d30e8e6916883f9d97f4a710f2e71ca
Omar Mohamed <omarcoptan9@gmail.com>
2026-06-16 17:06:49 +0300

    feat(theme): drop catppuccin theme
    Change-Id: I60e5503d52b9211d656c407145b6906410a2a52f

--- lisp/theme.el
+++ lisp/theme.el
@@ -1,9 +1,5 @@
 ;;; theme.el --- Nord theme -*- lexical-binding: t; -*-
 
-(use-package catppuccin-theme
-  :ensure (catppuccin-theme :host github :repo "catppuccin/emacs" :files ("*.el"))
-  :init
-  (setq catppuccin-flavor 'macchiato)) ;; or 'latte, 'macchiato, or 'mocha
 
 (use-package tron-legacy-theme
   :init
