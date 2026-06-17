3104b4617a676838c8551d547023b03d15c56c87
Omar Mohamed <omarcoptan9@gmail.com>
2026-06-17 03:39:41 +0300

    fix(theme): workaround broken theme in emacsclient
    Change-Id: I56ec3ce3d6bbc1a6a28b4bb78ec3e37de6e0720b

--- lisp/theme.el
+++ lisp/theme.el
@@ -1,9 +1,16 @@
 ;;; theme.el --- Nord theme -*- lexical-binding: t; -*-
 
+(defun oceanic/apply-theme (frame)
+  "Apply theme when a new emacsclient frame is created."
+  (with-selected-frame frame
+    (load-theme 'nordic-night t)))
+
 (use-package tron-legacy-theme)
 
 (use-package nordic-night-theme
   :init
+  (if (daemonp)
+	  (add-hook 'after-make-frame-functions #'oceanic/apply-theme))
   (load-theme 'nordic-night t))
 
 (provide 'theme)
