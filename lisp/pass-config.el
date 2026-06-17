f4e0d246f3e222507e15b6bd288a3f480016a44e
Omar Mohamed <omarcoptan9@gmail.com>
2026-06-17 03:01:24 +0300

    feat(pass): spawn window on the left
    Change-Id: I8ac886de79b9272b5d7a2c89f49ec9e48020ef02

--- lisp/pass-config.el
+++ lisp/pass-config.el
@@ -1,5 +1,14 @@
 ;;; pass-config.el --- password-store in Emacs -*- lexical-binding: t; -*-
-(use-package pass)
+
+(use-package pass
+  :config
+  (add-to-list 'display-buffer-alist
+               '("\\*Password-Store\\*"
+				 (display-buffer-in-side-window)
+				 (side . left)
+				 (window-width . 0.25)
+				 (window-parameters . ((no-other-window . nil)))
+				 (inhibit-same-window . t))))
 
 (provide 'pass-config)
 ;;; pass-config.el ends here
